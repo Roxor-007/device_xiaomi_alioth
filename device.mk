87f83e0d2119649b11d412d74ea7c16252a9e072
Alexander Winkowski <dereference23@outlook.com>
2022-08-05 11:48:32 +0000

    alioth: Fix AAPT config
    According to AOSP docs [1] apps should use xxhdpi resources
    for 440 dpi density.
    
    [1] https://developer.android.com/reference/android/util/DisplayMetrics#DENSITY_440
    
    Change-Id: Ib591e11a93b9814fabf772136096bde69e202e77

--- device.mk
+++ device.mk
@@ -36,7 +36,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Platform
 PRODUCT_BOARD_PLATFORM := kona
