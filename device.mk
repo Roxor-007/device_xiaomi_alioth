6c49828c7f48eb52487eb8411772b5f7b1c4e3c2
Trishiraj <gamerprince.exp@gmail.com>
2022-08-10 16:45:19 +0200

    Camera: Reflect some missed out camera changes
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- device.mk
+++ device.mk
@@ -217,6 +217,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
     libgui_vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    android.frameworks.sensorservice@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_PACKAGES += \
