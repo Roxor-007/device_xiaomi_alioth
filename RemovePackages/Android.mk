514d2d3b0efc5081dfdf445c131fc557546988f6
Roxor-007 <68855313+Roxor-007@users.noreply.github.com>
2023-07-21 05:47:58 -0700

    alioth: Add more packages to the debloater
    Some roms ship a lot of apps either with their own vendor or gapps. It's annoying af and we don't need them prebuilt.

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,11 +5,32 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    AudioFX \
+    Eleven \
+    Etar \
+    Jelly \
+    MusicFX \
+    Music \
+    Recorder \
+    Seedvault \
     Drive \
     GoogleCamera \
     Maps \
+    MiuiScanner \
     MyVerizonServices \
     OBDM_Permissions \
+    GoogleTTS \
+    arcore \
+    Videos \
+    MaestroPrebuilt \
+    AndroidAutoStubPrebuilt \
+    talkback \
+    SoundAmplifierPrebuilt \
+    DevicePolicyPrebuilt \
+    FilesPrebuilt \
+    TrichromeLibrary \
+    TrichromeWebView \
+    TrichromeChrome \
     obdm_stub \
     OemDmTrigger \
     OPScreenRecord \
@@ -21,6 +42,8 @@ LOCAL_OVERRIDES_PACKAGES := \
     MusicFX \
     SprintDM \
     SprintHM \
+    ViaBrowser \
+    Videos \
     VZWAPNLib \
     VzwOmaTrigger \
     YouTube \
